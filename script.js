e4f594cd9917b0e13da07e5d37391aaeacea837d
awaismughal8556-create <awaismughal8556@gmail.com>
2026-07-26 02:18:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+console.log("Portfolio Website Started...");
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if (document.documentElement.scrollTop > 200) {
+
+        topBtn.style.display = "block";
+
+    } else {
+
+        topBtn.style.display = "none";
+
+    }
+
+};
+
+topBtn.onclick = function () {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+};
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach((section) => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach((link) => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
+const counters = document.querySelectorAll(".count");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+
+            counter.innerText = Math.ceil(count + increment);
+
+            setTimeout(updateCounter,20);
+
+        }else{
+
+            counter.innerText = target + "+";
+
+        }
+
+    };
+
+    updateCounter();
+
+});
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Thank You! Your message has been received.");
+
+    form.reset();
+
+});
