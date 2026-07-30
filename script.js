522db58222be717ca95dee93fd5ffe977f6143ae
awaismughal8556-create <awaismughal8556@gmail.com>
2026-07-30 01:56:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -443,41 +443,6 @@ revealItems.forEach(item => {
 });
 
 
-/* =====================================================
-   PROJECT FILTER
-===================================================== */
-
-const filterButtons = document.querySelectorAll(".project-filter button");
-const projects = document.querySelectorAll(".project-card");
-
-filterButtons.forEach(button => {
-
-    button.addEventListener("click", () => {
-
-        filterButtons.forEach(btn => btn.classList.remove("active"));
-
-        button.classList.add("active");
-
-        const filter = button.dataset.filter;
-
-        projects.forEach(project => {
-
-            if (filter === "all" || project.classList.contains(filter)) {
-
-                project.style.display = "block";
-
-            } else {
-
-                project.style.display = "none";
-
-            }
-
-        });
-
-    });
-
-});
-
 
 /* =====================================================
    CONTACT FORM
